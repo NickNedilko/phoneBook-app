a1ac64cdc0168cca0b778c6412bf1b5f019126d5
NickNedilko <31307217+NickNedilko@users.noreply.github.com>
2024-07-30 11:30:16 +0300

    fix bugs with ts routes

--- src/components/RedirectRoutes.tsx
+++ src/components/RedirectRoutes.tsx
@@ -1,18 +1,39 @@
 import { Navigate } from "react-router-dom";
 import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
 import { useSelector } from "react-redux";
+import { FC } from "react";
 
-
-
-export const RestrictedRoute = ({ component, navigateTo = '/contacts' }) => {
-  const isLogged = useSelector(selectIsLoggedIn);
-
-  return isLogged ? <Navigate to={navigateTo} /> : component;
+interface RouteProps{
+  element:  JSX.Element,
+  redirectTo?: string
 }
 
-export const PrivateRoute = ({ component, navigateTo = '/' }) =>{
+export const RestrictedRoute: FC<RouteProps> = ({ element , redirectTo = '/contacts' }) => {
+  const isLogged = useSelector(selectIsLoggedIn);
+
+  return isLogged ? <Navigate to={redirectTo} /> : element;
+}
+
+export const PrivateRoute: FC<RouteProps> = ({ element, redirectTo = '/' }) =>{
   const isLogged = useSelector(selectIsLoggedIn);
   const isFetching = useSelector(selectIsRefreshing);
 
-  return !isLogged && !isFetching ? <Navigate to={navigateTo} /> : component;
+  return !isLogged && !isFetching ? <Navigate to={redirectTo} /> : element;
 }
+
+// import type { FC } from "react";
+// import { Outlet, Navigate } from "react-router-dom";
+
+
+// type TProps = {
+//   isAuthenticated?: boolean;
+//   redirectPath?: string;
+// };
+
+// export const PrivateRoutes: FC<TProps> = ({ isAuthenticated, redirectPath = '/' }) => {
+//   return isAuthenticated ? <Outlet /> : <Navigate to={redirectPath} />;
+// };
+
+// export const RestrictedRoute: FC<TProps> = ({ isAuthenticated, redirectPath = '/' }) => {
+//   return isAuthenticated ? <Navigate to={redirectPath}/> : <Outlet/> ;
+// };
